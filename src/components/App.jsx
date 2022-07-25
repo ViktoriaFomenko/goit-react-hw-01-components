9b1e48f73b9e3f82ff031d3a6f51a2231572948e
ViktoriaFomenko <aktavia@gmail.com>
2022-07-25 13:31:15 +0300

    add folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../../src/data/user.json';
 import data from '../../src/data/data.json';
+import transactions from '../../src/data/transactions.json';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
